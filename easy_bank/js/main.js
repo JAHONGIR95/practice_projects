55afa575d4ffbf515fe5e14367b0e73199ffeda2
Jahongir95 <sjahongir0032@gmail.com>
2020-12-17 18:22:45 +0500

    project is ready

--- easy_bank/js/main.js
+++ easy_bank/js/main.js
@@ -1,10 +1,12 @@
 var humburgerMenu = document.querySelector('#humburger');
 var overlay = document.querySelector('#overlay');
 var humburMenu = document.querySelector('#humbur-menu');
+var body = document.querySelector('body')
 
 
 humburgerMenu.addEventListener('click', function(){
     humburgerMenu.classList.toggle('clicked');
     overlay.classList.toggle('clicked');
     humburMenu.classList.toggle('opened');
+    body.classList.toggle('overflow');
 })
