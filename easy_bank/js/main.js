c1580c91591af2452edab7239d6d7d31ba1d00e8
Jahongir95 <sjahongir0032@gmail.com>
2020-12-17 16:58:57 +0500

    easybank design project is finished

--- easy_bank/js/main.js
+++ easy_bank/js/main.js
@@ -1,8 +1,10 @@
 var humburgerMenu = document.querySelector('#humburger');
 var overlay = document.querySelector('#overlay');
+var humburMenu = document.querySelector('#humbur-menu');
 
 
 humburgerMenu.addEventListener('click', function(){
     humburgerMenu.classList.toggle('clicked');
     overlay.classList.toggle('clicked');
+    humburMenu.classList.toggle('opened');
 })
