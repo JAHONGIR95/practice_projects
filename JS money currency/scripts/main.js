fcd800a409daeeda2f428b55982503badeadbab1
Jahongir95 <sjahongir0032@gmail.com>
2021-01-15 17:23:42 +0500

    task is finished

--- JS money currency/scripts/main.js
+++ JS money currency/scripts/main.js
@@ -1,3 +1,29 @@
-var currencyForm = document.querySelector('#currency');
-var insertMoney = document.querySelector('#amount');
+var currencyForm = document.querySelector('#currency'); //form
+var select = document.querySelector('#currency-choosing'); //select options
+var dollar = document.querySelector('#toSum'); // natija chiqadigan joy
+var exchange = document.querySelector('#submit'); // amal button
+var insertMoney = document.querySelector('#amount'); // user input
 
+
+function toChange(pul) {
+
+    if (pul > 0) {
+        return dollar.innerHTML = pul * select.value;
+    }
+        else {
+            return 'Summani kiriting';
+    }
+}
+
+insertMoney.addEventListener('keyup', function(e){
+    e.preventDefault();
+
+    toChange(insertMoney.value)
+})
+
+
+select.addEventListener('change', function(e){
+    e.preventDefault();
+    
+    toChange(insertMoney.value);
+})
