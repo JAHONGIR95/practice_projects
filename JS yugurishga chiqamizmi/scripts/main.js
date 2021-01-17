12ed402f834958a4f994dafe3ffd6647081f1a40
Jahongir95 <sjahongir0032@gmail.com>
2021-01-17 23:42:38 +0500

    events is adding

--- JS yugurishga chiqamizmi/scripts/main.js
+++ JS yugurishga chiqamizmi/scripts/main.js
@@ -4,6 +4,7 @@ var result = document.querySelector('#result');
 var temprature = document.querySelector('#temprature');
 var raining = document.querySelector('#raining');
 var gym = document.querySelector('#gym');
+var ask = document.querySelector('#ask');
 
 // Havo haroratini kiritish
 
@@ -24,48 +25,67 @@ function tempChecking() {
     }
 }
 
-// Yomg'ir yog'ayotganimi tekshirish
 
-raining.addEventListener('change', function (e) {
-    e.preventDefault();
-
-    rainings();
-
-})
-
-var rain = false; //checkbox ning dastlabki qiymatiga false berdim 
-
-function rainings() {
-
-    rain = !rain;
+function tekshirish(){
+    // console.log(rainings());
     
-    if (rain == true) {
-        result.innerHTML = (`<span style="color: red;"> YO'Q</span>`);
-    } else {
-        result.innerHTML = (`<span style="color: green;"> HA</span>`);
-    }
+    var rain = false; //checkbox ning dastlabki qiymatiga false berdim 
+    var zal = false; // gym ning checkbox sining dastlabki qiymatiga false berdim
+    
+    ask.addEventListener('change', function(e){
+        e.preventDefault();
+        
+        
+        // Yomg'ir yog'ayotganimi tekshirish
+        raining.addEventListener('click', function (e) {
+            e.preventDefault();
+            
+            rainings();
+        })
+        
+        
+        
+        // is GYM open? checking
+        gym.addEventListener('click', function (e) {
+            e.preventDefault();
+            
+            gymOpen();
+        })
+        
+        function rainings() {
+            rain = !rain;
+            
+            // console.log(rain);
+            
+            if (rain == true) {
+                result.innerHTML = (`<span style="color: red;"> YO'Q</span>`);
+            } else {
+                result.innerHTML = (`<span style="color: green;"> HA</span>`);
+            }
+        }
+        
+        function gymOpen(){
+            
+            zal = !zal;
+        
+            // console.log(zal);
+        
+            if(zal == true){
+                result.innerHTML = (`<span style="color: red;"> YO'Q</span>`);
+            } else {
+                result.innerHTML = (`<span style="color: green;"> HA</span>`);
+            }
+        }
+
+
+        // if(rain == true && zal == true){
+        //     alert('yuguramiz');
+        // }
+        
+        console.log(rain);
+        console.log(zal);
+    })
+    
 }
+tekshirish();
 
-// is GYM open? checking
-
-gym.addEventListener('change', function (e) {
-    e.preventDefault();
-
-    gymOpen();
-
-})
-
-var zal = false; // gym ning checkbox sining dastlabki qiymatiga false berdim
-
-function gymOpen(){
-
-    zal = !zal;
-
-    // console.log(zal);
-
-    if(zal == true){
-        result.innerHTML = (`<span style="color: green;"> HA</span>`);
-    } else {
-        result.innerHTML = (`<span style="color: red;"> YO'Q</span>`);
-    }
-}
