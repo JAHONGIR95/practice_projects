c56c0e1f2071a194e13281a35170cd0da00b80cd
Jahongir95 <sjahongir0032@gmail.com>
2021-01-18 18:15:24 +0500

    ids and user inputs are taken as variables

--- JS kim birinchi boradi/scripts/main.js
+++ JS kim birinchi boradi/scripts/main.js
@@ -1,1 +1,5 @@
-var form = document.querySelector('#form');
+var form = document.querySelector('#form'); // form
+var masofa = document.querySelector('#distance'); // user input
+var result = document.querySelector('#result'); //natija oynasi
+
+
