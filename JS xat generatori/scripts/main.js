9a42996f16359ea659bea30c1179de36e1ab7639
Jahongir95 <sjahongir0032@gmail.com>
2021-01-18 12:02:48 +0500

    xat generator is also finished

--- JS xat generatori/scripts/main.js
+++ JS xat generatori/scripts/main.js
@@ -1,6 +1,33 @@
 var result = document.querySelector('#result');
-console.log(result);
+var kimga = document.querySelector('#kimga');
+// console.log(result);
 
-function onamga(){
-    result.innerHTML = (`hammaga Salom`);
+function onamga() {
 }
+
+kimga.addEventListener('change', function (e) {
+    e.preventDefault();
+    console.log(kimga.value);
+
+    switch (true) {
+        case (kimga.value == 'Onajon'):
+            result.innerHTML = (`Assalomu alaykum, ${kimga.value}`);
+            break;
+
+        case (kimga.value == `Do'stim`):
+            result.innerHTML = (`Assalomu alaykum, ${kimga.value}`);
+            break;
+
+        case (kimga.value == 'Men...'):
+            result.innerHTML = (`Assalomu alaykum, ${kimga.value}`);
+            break;
+
+        case (kimga.value == 'Sevgilim'):
+            result.innerHTML = (`Salom ${kimga.value}`);
+            break;
+
+
+    }
+
+})
+
