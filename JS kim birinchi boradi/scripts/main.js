5d9fd795434397bbae49f3e5d816c7c14f2107b1
Jahongir95 <sjahongir0032@gmail.com>
2021-01-18 21:20:56 +0500

    js code for piyoda is finished

--- JS kim birinchi boradi/scripts/main.js
+++ JS kim birinchi boradi/scripts/main.js
@@ -3,3 +3,32 @@ var masofa = document.querySelector('#distance'); // user input
 var result = document.querySelector('#result'); //natija oynasi
 
 
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    
+    
+    piyoda();
+})
+
+function piyoda() {
+    var distance = parseInt(masofa.value);
+    
+    var time =  (distance / 60);
+    var fixedTime = time.toFixed(2);
+    totalTime = fixedTime * 60;
+    
+    
+    minutDaqiqa();
+}
+
+function minutDaqiqa(){
+    
+    if(totalTime % 60 == 0){
+        result.textContent = time + ' soat';
+    } else {
+        var minut = (totalTime % 60).toFixed(0);
+        var soat = totalTime / 60;
+        
+        result.innerHTML = Math.floor(soat) + ' soat'  + '<br>' + minut + ' daqiqa';
+    }
+}
