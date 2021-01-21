18adcfd25528ee3d3440a8ebcb0a2155742b750c
Jahongir95 <sjahongir0032@gmail.com>
2021-01-21 23:28:10 +0500

    3 function task is started to prictise javaScript

--- JS masala/main.js
+++ JS masala/main.js
@@ -25,3 +25,39 @@ function square(n){
 	}
 }
 square(m);
+
+// if(!window.localStorage.getItem('username')){
+// 	var usernamePrompt = window.prompt('Ismingizni yozing...');
+
+// 	window.localStorage.setItem('username', usernamePrompt);
+// } else {
+// 	alert(`Salom ${window.localStorage.getItem('username')}`);
+// }
+
+// window.addEventListener('online', checkStatus);
+// window.addEventListener('offline', checkStatus);
+
+// function checkStatus(){
+// 	if(window.clientInformation.onLine){
+
+// 	} else {
+// 		alert('Internet ochib qoldi brat!');
+// 	}
+// }
+
+var num = [4, 3, 11, 45, 2, 6, 0, 1];
+var a;
+var b;
+
+for(i = 0; i < num.length; i++){
+	if(num[i] < num[i+1]){
+		a = num[i];
+		num[i] = num[i+1];
+		num[i+1] = a;
+	}
+	else{
+		
+	}
+	
+}
+console.log(num);
