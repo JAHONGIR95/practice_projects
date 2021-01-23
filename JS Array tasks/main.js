e75d3dd9fae122483cea9e3878d580ae1648baac
Jahongir95 <sjahongir0032@gmail.com>
2021-01-23 13:39:27 +0500

    first task of array task is finished

---
+++ JS Array tasks/main.js
@@ -0,0 +1,20 @@
+console.log('*************** task 1 *****************');
+var mark = [1, 2, 3, 4];
+s = 0;
+avarage = 0;
+function avarageNumber(mark){
+    for(var i = 0; i < mark.length; i++){
+        s += mark[i];
+    }
+    average = s / mark.length;
+    return 'Arrayning o\'rta qiymati: ' + average;
+}
+console.log(avarageNumber(mark));
+console.log('*************** task 2 *****************');
+
+var mark = [1, -1, 2, -5, 6];
+function positiveNumbers(mark){
+    for(var i = 0; i < mark.length; i++){
+        
+    }
+}
