cd4292b5abd2713d5068728a7af3beefb5d87c09
Jahongir95 <sjahongir0032@gmail.com>
2021-01-27 12:28:01 +0500

    project 6 js logic is creating

---
+++ JS beginners projects/project 6/scripts/main.js
@@ -0,0 +1,42 @@
+var imageSlider = document.querySelector('.image-slider');
+
+
+// console.log(imageSlider);
+var i = 0;
+setInterval(function(){
+    i++;
+    console.log(num);
+    
+    
+    
+    
+    imageSlider.style.backgroundImage = `url('../images/car${num}.jpg')`
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    var num = i % 4;
+}, 2000)
