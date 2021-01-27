9612ef3dcfc14e5b115a4a1d696dbe90b0572d3a
Jahongir95 <sjahongir0032@gmail.com>
2021-01-27 12:49:31 +0500

    project  6 image slider is finished

--- JS beginners projects/project 6/scripts/main.js
+++ JS beginners projects/project 6/scripts/main.js
@@ -1,42 +1,20 @@
 var imageSlider = document.querySelector('.image-slider');
 
-
-// console.log(imageSlider);
 var i = 0;
+
 setInterval(function(){
-    i++;
-    console.log(num);
-    
-    
-    
-    
-    imageSlider.style.backgroundImage = `url('../images/car${num}.jpg')`
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    bgChanger();
+}, 3000)
+
+setTimeout(() => {
+    // this code gives us first 3 second image
+    imageSlider.style.backgroundImage = `url('../images/car3.jpg')`   
+}, 0);
+
+function bgChanger(){
+    // this algorithm gives us same index number repeatedly
     var num = i % 4;
-}, 2000)
+    i++;
+    // background image changes every 3 second repeatedly
+    imageSlider.style.backgroundImage = `url('../images/car${num}.jpg')`  
+}
