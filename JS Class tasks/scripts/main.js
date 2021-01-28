cd3a8382b77afad7e21b718ac4cf13fc719c72fb
Jahongir95 <sjahongir0032@gmail.com>
2021-01-28 10:47:53 +0500

    designers web site design is creating

--- JS Class tasks/scripts/main.js
+++ JS Class tasks/scripts/main.js
@@ -1,6 +1,42 @@
-var form = document.querySelector('#form');
-var colorInput = document.querySelector('#input');
 
-form.addEventListener('submit', function(){
-    document.body.style.backgroundColor = colorInput.value;
-})
+var i = 0;
+var j = 0;
+var k = 0;
+
+function colorRising(){
+    document.body.style.backgroundColor = `rgba(${i}, ${j}, ${k})`;
+    // clearInterval()
+}
+
+const intervalId = setInterval(() => {
+    colorRising();
+    if(i >= 255){
+        clearInterval(intervalId);
+        console.log(i);
+        console.log('yes');
+    }
+    else{   
+        i++;
+        console.log(i);
+    }
+}, 20);
+
+setTimeout(() => {
+    const intervalIdJ = 
+        setInterval(() => {
+            colorRising();
+            if(j >= 255){
+                clearInterval(intervalIdJ);
+                console.log(j);
+                console.log('j');
+            }
+            else{   
+                j++;
+                console.log(j);
+            }
+        }, 20);
+}, 5120);
+
+
+
+
