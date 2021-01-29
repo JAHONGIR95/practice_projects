8dd948e39c65cb875307c138f8a2788b1ab5f136
Jahongir95 <sjahongir0032@gmail.com>
2021-01-29 18:32:22 +0500

    loop 1 task is finished

--- JS beginners projects/js loop 1/scripts/main.js
+++ JS beginners projects/js loop 1/scripts/main.js
@@ -1,9 +1,26 @@
 var buttons = document.querySelectorAll('.buttons');
 var select = document.querySelector('#select');
 
-console.log(buttons);
-buttons.forEach(item => {
-    item.addEventListener('click', function(){
-        if()
+// console.log();
+
+function buttonSwitcher(num) {
+    select.value = num;
+
+    buttons.forEach((item) => {
+        if (num != item.dataset.orderNum) {
+            item.classList.remove('active')
+        } else {
+            item.classList.add('active')
+        }
     })
+}
+
+buttons.forEach(item => {
+    item.addEventListener('click', function () {
+        buttonSwitcher(item.dataset.orderNum)
+    })
+})
+
+select.addEventListener('change', function () {
+    buttonSwitcher(select.value);
 })
