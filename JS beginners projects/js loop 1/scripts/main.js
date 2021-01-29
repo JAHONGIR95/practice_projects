3fcd299ecff762a294c30b8503255fdf9531f3a7
Jahongir95 <sjahongir0032@gmail.com>
2021-01-29 23:32:55 +0500

    loop 1 is priticed for better performance

--- JS beginners projects/js loop 1/scripts/main.js
+++ JS beginners projects/js loop 1/scripts/main.js
@@ -1,26 +1,24 @@
 var buttons = document.querySelectorAll('.buttons');
 var select = document.querySelector('#select');
 
-// console.log();
+function buttonSwitcher(arg){
 
-function buttonSwitcher(num) {
-    select.value = num;
-
-    buttons.forEach((item) => {
-        if (num != item.dataset.orderNum) {
-            item.classList.remove('active')
+    select.value = arg;
+    buttons.forEach(item => {
+        if(arg != item.dataset.orderNum){
+            item.classList.remove('active');
         } else {
-            item.classList.add('active')
+            item.classList.add('active');
         }
     })
 }
 
 buttons.forEach(item => {
-    item.addEventListener('click', function () {
-        buttonSwitcher(item.dataset.orderNum)
+    item.addEventListener('click', function(){
+        buttonSwitcher(item.dataset.orderNum);
     })
 })
 
-select.addEventListener('change', function () {
+select.addEventListener('change', function(){
     buttonSwitcher(select.value);
 })
