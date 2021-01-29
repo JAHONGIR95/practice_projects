3707a1045aed0435fa756b8671c3b776636fae1b
Jahongir95 <sjahongir0032@gmail.com>
2021-01-29 17:38:10 +0500

    html structure of the project is finished

---
+++ JS beginners projects/js loop 1/scripts/main.js
@@ -0,0 +1,9 @@
+var buttons = document.querySelectorAll('.buttons');
+var select = document.querySelector('#select');
+
+console.log(buttons);
+buttons.forEach(item => {
+    item.addEventListener('click', function(){
+        if()
+    })
+})
