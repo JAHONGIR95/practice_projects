f91c0febe349df1b2a406e71771d642ca8d9d51c
Jahongir95 <sjahongir0032@gmail.com>
2021-02-04 14:30:52 +0500

    tilt project is creating

--- JS beginners projects/loader/scripts/main.js
+++ JS beginners projects/loader/scripts/main.js
@@ -1,16 +1,30 @@
-let block = document.querySelector('.block');
+// asosiy blockni DOM dan chaqirib olish
+let frame = document.querySelector('.frame');
 
-window.addEventListener('scroll', function () {
-    if (pageYOffset >= 200) {
-        block.classList.add('active');
-    } else {
-        block.classList.remove('active');
-    }
+// DOM da yaratilgan blockning eni ni topish
+const width = frame.clientWidth;
+// DOM da yaratilgan blockning bo'yi ni topish
+const height = frame.clientHeight;
+
+// event DOM dagi block ustida animatsiya uchun function
+frame.addEventListener('mousemove', function(e){
+
+    // mouse ni DOM dagi positsiyasini topish X bo'yicha
+    const xPosition = e.layerX;
+    // mouse ni DOM dagi positsiyasini topish Y bo'yicha
+    const yPosition = e.layerY;
+
+    // Y bo'yicha qancha burilishini hisoblash
+    const yRotation = 50 * ((xPosition - width / 2) / width);
+    // X bo'yicha qancha burilishini hisoblash
+    const xRotation = -50 * ((yPosition - height / 2) / height);
+
+
+    this.style.transform = `rotateX(${xRotation}deg) rotateY(${yRotation}deg) `;
+    
 })
 
-document.querySelector(".block").addEventListener("click", () => {
-    document.querySelector("html").style.scrollBehavior = "smooth";
-    setTimeout(() => {
-        document.querySelector("html").style.scrollBehavior = "unset";
-    }, 5000);
-});
+frame.addEventListener('mouseleave', function(e){
+    this.style.transform = `rotateX(0) rotateY(0)`;
+})
+
