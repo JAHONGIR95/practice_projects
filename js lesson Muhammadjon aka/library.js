329a75cce44f70d8edd14abd401a079ed8e542f7
Jahongir95 <sjahongir0032@gmail.com>
2021-03-04 13:15:51 +0500

    car class task is finished

--- js lesson Muhammadjon aka/library.js
+++ js lesson Muhammadjon aka/library.js
@@ -15,5 +15,35 @@
 
 // export {birthYear, Human}
 
+class Cars{
+    constructor(color, carAge, model, weight){
+        this.color = color;
+        this.carAge = carAge;
+        this.model = model;
+        this.weight = weight;
+    }
 
+    colorCar(){
+        return this.color;
+    }
+
+    carYear(){
+        return `Manufactured year is ${this.carAge}`;
+    }
+
+    modelCar(){
+        return this.model;
+    }
+
+    weightCar(){
+        return this.weight;
+    }
+}
+
+let color = 'white';
+let carAge = 2008;
+let model = 'BMW';
+let weight = '800kg';
+
+export {color, carAge, model, weight, Cars}
 
