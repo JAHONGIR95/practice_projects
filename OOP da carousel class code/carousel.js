413818dba0535510a22be2b548651ddf27a44ec5
Jahongir95 <sjahongir0032@gmail.com>
2021-03-16 18:25:39 +0500

    main class is created

--- OOP da carousel class code/carousel.js
+++ OOP da carousel class code/carousel.js
@@ -3,6 +3,12 @@ class Carousel {
         this.carousel = document.querySelector(options.parentSelector);
         this.carouselItems = document.querySelectorAll(`${options.parentSelector}-item`);
         this.time = options.time;
-        
+        this.width = options.size.width + 'px';
+        this.height = options.size.height + 'px';
+        this.activeElement = options.activeElement;
+
+        this.carouselItems.forEach((item, index) => {
+            item.style.width = this.width;
+        })
     }
 }
