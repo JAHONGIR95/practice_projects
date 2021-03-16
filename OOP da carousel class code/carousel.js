65284237701a44519dbcd219c5e2f70ffe3943d6
Jahongir95 <sjahongir0032@gmail.com>
2021-03-16 23:52:29 +0500

    carousel js code is developing

--- OOP da carousel class code/carousel.js
+++ OOP da carousel class code/carousel.js
@@ -9,6 +9,34 @@ class Carousel {
 
         this.carouselItems.forEach((item, index) => {
             item.style.width = this.width;
+            item.style.height = this.height;
+            item.style.objectFit = 'cover';
+
+
         })
+        this.carouselChanger();
+    }
+
+    carouselFader(activeElementIndex){
+        this.carouselItems.forEach((item, index) => {
+            if(index == activeElementIndex) {
+                item.style.display = 'block';
+            } else {
+                item.style.display = 'none';
+            }
+        })
+    }
+
+    carouselChanger(){
+        let activeElementIndex = 0;
+
+        setInterval(() => {
+            if(activeElementIndex < this.carouselItems.length - 1){
+                activeElementIndex++;
+
+            } else {
+                activeElementIndex = 0;
+            }
+        }, this.time);
     }
 }
